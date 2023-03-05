86f7f7c256c5f245a083b8083485afb8d5a8717f
DevOfHockey <liam.conkin@gmail.com>
2023-03-05 13:18:14 -0800

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,7 @@ export default function Head() {
     <>
       <title>Contentify</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Developed by Liam Conkin" />
+      <meta name="description" content="A Chakra UI Next.js landing page utilizing Firebase authentification sign in. Developed by Liam Conkin" />
       <link rel="icon" href="/favicon.ico" />
     </>
   )
